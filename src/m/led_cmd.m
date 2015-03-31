acdc567ac0028a5c40231852d2e2bc931af3e026
Patrick Martin <prof.pmartin10@gmail.com>
2015-03-31 12:31:19 -0400

    Adding matlab-side suppport

---
+++ src/m/led_cmd.m
@@ -0,0 +1,20 @@
+
+%  Author: Samuel <snelso15@ycp.edu>
+%  function to wrap led toggle in matlab 
+%  args:
+%  pc - physicloudclient object 
+%  ids - robot ids that the command is to be sent
+%  led - 1 or 2 to indicate which led
+%  color - either 'red', 'green', or 'off'
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function[ret] = led_cmd(pc, ids, led, color)
+  if led ~= 1 & led ~=2
+    disp('ERROR:  LED must be 1 or 2');
+  else if ~strcmp('red', color) & ~strcmp('green', color) & ~strcmp('off', color)
+    disp('ERROR: color must be "red", "green", or "off"');
+  else
+    pc.led(ids, led, color);
+    ret = 1;
+  end
+return 
---
+++ src/m/sound_cmd.m
@@ -0,0 +1,24 @@
+
+%  Author: Samuel <snelso15@ycp.edu>
+%  function to wrap kobuki sound play in matlab 
+%  args:
+%  pc - physicloudclient object 
+%  ids - robot ids that the command is to be sent
+%  sound -
+%  	0 for ON sound
+%		1 for OFF sound
+%		2 for RECHARGE sound
+%		3 for BUTTON sound
+%		4 for ERROR sound
+%		5 for CLEANINGSTART sound
+%		6 for CLEANINGEND sound
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function[ret] = sound_cmd(pc, ids, sound)
+  if sound < 0 | sound > 6 
+    disp('ERROR: sound tag must be integer in rage 0-6');
+  else
+    pc.led(ids, led, color);
+    ret = 1;
+  end
+return 
