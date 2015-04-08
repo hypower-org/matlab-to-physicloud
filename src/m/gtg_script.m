697ed24bbccaf8d42c12627a97c863a8bbcd320b
Patrick Martin <prof.pmartin10@gmail.com>
2015-04-08 11:18:32 -0400

    stable version across systems

--- src/m/gtg_script.m
+++ src/m/gtg_script.m
@@ -2,12 +2,13 @@ idx = 0;
 g_x = 0;
 g_y = 0;
 while idx < 200
-  jdata = pc.getData('robot1');
-  x = jdata(1);
-  y = jdata(2);
-  t = jdata(3);
-  [v, w] = gtg(x, y, t, g_x, g_y);
-  pc.drive(v, w);
+  [x1, y1, t1, rest] = get_state(pc, 'robot1');
+  [x2, y2, t2, rest] = get_state(pc, 'robot2');
+  [x3, y3, t3, rest] = get_state(pc, 'robot3');
+  [v1, w1] = gtg(x1, y1, t1, g_x, g_y);
+  [v2, w2] = gtg(x2, y2, t2, g_x, g_y);
+  [v3, w3] = gtg(x3, y3, t3, g_x, g_y);
+  drive_cmd(pc, ids, [v1, v2, v3], [w1, w2, w3]);
   pause(0.1);
   idx = idx + 1;
 end 
