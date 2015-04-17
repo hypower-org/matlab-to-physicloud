e327b96d714dc8f181e518145c39dd46bb31d583
snelso15 <snelso15@ycp.edu>
2015-04-17 13:36:56 -0400

    Zeroing gyro on each reset_coords command

--- src/clojure/project.clj
+++ src/clojure/project.clj
@@ -5,15 +5,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  ;[hypower-org/physicloud "0.2.0"]
-                 [pcloud "0.2.2"] 
+                 [pcloud "0.2.3"] 
                  [hypower-org/watershed "0.1.5"]
                  [manifold "0.1.0-beta10"]
                  [pcc "1.0.0"]
-                 ;[jkobuki "1.2.0"]
-                 ;[phidget "1.0.0"]
-                 ;[jssc "2.8.0"]
+                 [jkobuki "1.2.0"]
+                 [phidget "1.0.0"]
+                 [jssc "2.8.0"]
                  [seesaw "1.4.5"]]
-  :main matlab-physicloud.matlab-server
-  :aot [matlab-physicloud.matlab-server])
+  :main matlab-physicloud.moving-imu-client
+  :aot [matlab-physicloud.moving-imu-client]
+ )
  
  
